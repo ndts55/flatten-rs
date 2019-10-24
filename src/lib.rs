f1ee9529a2d82a6b78a588e5205d6fdd51d7e893
Nicolas Torres <nicolastorres@mailbox.org>
2019-10-24 21:21:22 +0200

    split main into main and lib

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::{self, env, fs, io, path};
+
+pub fn flatten(dir: &path::Path) -> io::Result<()> {
+    if dir.is_dir() {
+        env::set_current_dir(dir).unwrap();
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            let path_name = path.file_name().unwrap().to_str().unwrap().to_owned();
+            if path.is_dir() {
+                env::set_current_dir(&path).unwrap();
+                for child in fs::read_dir(&path)? {
+                    let child = child?;
+                    let file_name = child.file_name();
+                    let name = file_name.to_str().unwrap();
+                    let new_name = path_name.clone() + " - " + name;
+                    println!("{}", new_name);
+                    fs::rename(name, "../".to_string() + &new_name)?;
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{self, env, fs, io, path};
+use std::{self, path};
 use structopt::StructOpt;
+
 
 #[derive(StructOpt)]
 struct Args {
@@ -9,30 +10,6 @@ struct Args {
 
 fn main() {
     let args = Args::from_args();
-    let res = flatten(&args.path);
+    let res = flatten::flatten(&args.path);
     println!("{:?}", res);
-}
-
-fn flatten(dir: &path::Path) -> io::Result<()> {
-    if dir.is_dir() {
-        env::set_current_dir(dir).unwrap();
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            let path_name = path.file_name().unwrap().to_str().unwrap().to_owned();
-            if path.is_dir() {
-                env::set_current_dir(&path).unwrap();
-                for child in fs::read_dir(&path)? {
-                    let child = child?;
-                    let file_name = child.file_name();
-                    let name = file_name.to_str().unwrap();
-                    let new_name = path_name.clone() + " - " + name;
-                    println!("{}", new_name);
-                    fs::rename(name, "../".to_string() + &new_name)?;
-                }
-            }
-        }
-    }
-
-    Ok(())
 }
